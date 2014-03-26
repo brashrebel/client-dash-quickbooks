5953a2a6f0eabb33771f959d9b1c5489492fe513
Kyle <kyle@realbigmarketing.com>
2014-03-26 16:21:13 -0400

    First commit
    Just one file so far.

---
+++ client-dash-quickbooks.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Client Dash Quickbooks Add-on
+Description: Integrates Intuit's Quickbooks Online information with Client Dash
+Version: 0.1
+Author: Kyle Maurer
+Author URI: http://realbigmarketing.com/staff/kyle
+*/
+
+?>
