d25324d1bbc2864d0a05a6d115a60a632a2f8730
Kyle <kyle@realbigmarketing.com>
2014-06-15 01:26:32 -0400

    Removed testing stuff and started class
    Ditched old crap and started over building this with a class

--- client-dash-quickbooks.php
+++ client-dash-quickbooks.php
@@ -6,23 +6,20 @@ Version: 0.1
 Author: Kyle Maurer
 Author URI: http://realbigmarketing.com/staff/kyle
 */
-// Add the Billing tab under the account page
-function cdq_tab() {
-$cdq_active_tab = $_GET['tab'];
-?>
-<a href="?page=account&tab=billing" class="nav-tab <?php echo $cdq_active_tab == 'billing' ? 'nav-tab-active' : ''; ?>">Billing</a>
-<?php }
-add_action('cd_account_tabs_after', 'cdq_tab');
 
-// Modify the tab content path to include file from this plugin
-function cdq_path($cd_tab_path) {
-	$cdq_cur_tab = $_GET['tab'];
-	if ($cdq_cur_tab == 'billing') {
-	$cd_tab_path = plugin_dir_path( __FILE__ ).'layout/billing-tab.php';
-	return $cd_tab_path;
-	} else {
-		return $cd_tab_path;
+class CDQBO {
+
+	public function __construct() {
+		add_action('admin_notices', array( $this, 'notices' ) );
+	}
+// Notices for if CD is not active
+	public function notices() {
+	if (!is_plugin_active( 'client-dash/client-dash.php' )) {
+	echo '<div class="error">Client Dash Quickbooks Online requires <b>Client Dash</b>. Please install and activate <b>Client Dash</b> to continue using.</div>';
 	}
 }
-add_filter('cd_tab_path', 'cdq_path');
-?>
+
+// Add the Billing tab under the account page
+
+}
+$cdqbo = new CDQBO;
--- layout/billing-tab.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo 'billing info';
-?>
