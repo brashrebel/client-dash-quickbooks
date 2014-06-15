463cef734dc11b075c8ac495516e0d664251db37
Kyle <kyle@realbigmarketing.com>
2014-06-15 01:36:21 -0400

    Created tab and setup contents
    We are now ready to begin fetching data.

--- client-dash-quickbooks.php
+++ client-dash-quickbooks.php
@@ -11,15 +11,26 @@ class CDQBO {
 
 	public function __construct() {
 		add_action('admin_notices', array( $this, 'notices' ) );
+		add_filter('cd_tabs', array( $this, 'add_tab' ) );
+		add_action('cd_account_billing_tab', array( $this, 'tab_contents' ) );
 	}
-// Notices for if CD is not active
+
+	// Notices for if CD is not active
 	public function notices() {
-	if (!is_plugin_active( 'client-dash/client-dash.php' )) {
-	echo '<div class="error">Client Dash Quickbooks Online requires <b>Client Dash</b>. Please install and activate <b>Client Dash</b> to continue using.</div>';
+		if (!is_plugin_active( 'client-dash/client-dash.php' )) {
+		echo '<div class="error">Client Dash Quickbooks Online requires <b>Client Dash</b>. Please install and activate <b>Client Dash</b> to continue using.</div>';
+		}
 	}
-}
 
-// Add the Billing tab under the account page
+	// Add the billing tab
+	public function add_tab( $tabs ) {
+	$tabs['account']['Billing'] = 'billing';
+	return $tabs;
+	}
 
+	// Insert the tab contents
+	public function tab_contents() {
+		echo 'test';
+	}
 }
 $cdqbo = new CDQBO;
